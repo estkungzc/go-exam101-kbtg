3478b569fac1a7e240356bfa830dfe746396bc02
Est Chairat <est.kungzc@gmail.com>
2021-06-30 14:42:08 +0700

    add go_exam_3

--- go_exam_3/main.go
+++ go_exam_3/main.go
@@ -1,5 +1,56 @@
-package go_exam_3
+package main
+
+import (
+	"flag"
+	"fmt"
+	"sync"
+)
 
 func main() {
+	idRange := flag.Int("id_range", 0, "range id to call api")
+	empId := flag.String("employee_id", "", "employee id")
 
+	routines := flag.Int("routines", 10, "routines threads")
+
+	flag.Parse()
+
+	if *idRange <= 0 || *idRange >= 250 {
+		panic(fmt.Errorf("id_range must be in 0 < id_range < 250"))
+	}
+
+	if *empId == "" {
+		panic(fmt.Errorf("employee_id is required"))
+	}
+
+	if *routines <= 0 || *routines > 50 {
+		*routines = 10
+	}
+
+	pipelineOut := make(chan Photo)
+
+	defer close(pipelineOut)
+
+	var wg sync.WaitGroup
+	wg.Add(*idRange)
+
+	var photoArr []Photo
+	//Channel get output
+	go func() {
+		for v := range pipelineOut {
+			fmt.Println("data in channel :", v)
+			photoArr = append(photoArr, v)
+			wg.Done() // for handle GetDataFromPhotoAPI success before append data (change from GetDataFromPhotoAPI)
+		}
+	}()
+
+	//Loop to feed input to worker
+	for i := 1; i <= *idRange; i++ {
+		go GetDataFromPhotoAPI(i, &wg, pipelineOut, *empId)
+	}
+
+	wg.Wait()
+	fmt.Println("call api complete ")
+	//time.Sleep(1*time.Second)
+	fmt.Println(len(photoArr))
+	WriteXlsxPhoto(*empId,"gobasic_exam3", photoArr)
 }
---
+++ go_exam_3/photo.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tealeg/xlsx"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type Photo struct {
+	AlbumId      int    `json:"albumId"`
+	Id           int    `json:"id"`
+	Title        string `json:"title"`
+	Url          string `json:"url"`
+	ThumbnailUrl string `json:"thumbnailUrl"`
+
+	GoRoutinesId int
+	EmployeeId string
+}
+
+
+func GetDataFromPhotoAPI(id int, wg *sync.WaitGroup, pipelineOut chan Photo, empId string) {
+	url := fmt.Sprintf("%s/%d", "https://jsonplaceholder.typicode.com/photos", id)
+
+	httpReq, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Println("error json unmarshall : ", err)
+		pipelineOut <- Photo{
+			Id: id,
+		}
+
+		return
+	}
+
+	tr := &http.Transport{
+		MaxIdleConns:       10,
+		IdleConnTimeout:    30 * time.Second,
+		DisableCompression: true,
+	}
+	client := &http.Client{Transport: tr}
+
+	resp, err := client.Do(httpReq)
+	if err != nil {
+		fmt.Println("error call ", err)
+		pipelineOut <- Photo{
+			Id: id,
+		}
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var respData Photo
+	if err := json.NewDecoder(resp.Body).Decode(&respData); err != nil {
+		fmt.Println("error decode json user :", err)
+		pipelineOut <- Photo{
+			Id: id,
+		}
+		return
+	}
+
+	respData.GoRoutinesId = id
+	respData.EmployeeId = empId
+	pipelineOut <- respData
+}
+
+func WriteXlsxPhoto(fileName string, sheetName string, photoArr []Photo) {
+	var file *xlsx.File
+	var sheet *xlsx.Sheet
+	var row *xlsx.Row
+	//var cell *xlsx.Cell
+	var err error
+
+	file = xlsx.NewFile()
+	sheet, err = file.AddSheet(sheetName)
+	if err != nil {
+		fmt.Println("error cannot add sheet name")
+		return
+	}
+
+	row = sheet.AddRow()
+	row.AddCell().SetValue("GoRoutineId")
+	row.AddCell().SetValue("EmployeeId")
+
+	row.AddCell().SetValue("AlbumId")
+	row.AddCell().SetValue("Id")
+	row.AddCell().SetValue("Title")
+	row.AddCell().SetValue("Url")
+	row.AddCell().SetValue("ThumbnailUrl")
+
+	for _, po := range photoArr {
+		row = sheet.AddRow()
+		row.AddCell().SetValue(po.GoRoutinesId)
+		row.AddCell().SetValue(po.EmployeeId)
+		row.AddCell().SetValue(po.AlbumId)
+		row.AddCell().SetValue(po.Id)
+		row.AddCell().SetValue(po.Title)
+		row.AddCell().SetValue(po.Url)
+		row.AddCell().SetValue(po.ThumbnailUrl)
+	}
+	filePath := fmt.Sprintf("%s.xlsx", fileName)
+	err = file.Save(filePath)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+}
